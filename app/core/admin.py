c818a7133395f264a6071042787282b2fbc16571
Lucas Begnini <lucasbegnini@gmail.com>
2023-05-25 13:47:56 -0300

    include drf spectacular openapi

--- app/core/admin.py
+++ app/core/admin.py
@@ -16,6 +16,7 @@ class UserAdmin(BaseUserAdmin):
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
             _('Permissions'),
             {
@@ -40,7 +41,7 @@ class UserAdmin(BaseUserAdmin):
                 'is_active',
                 'is_staff',
                 'is_superuser',
-            )
+            ),
         }),
     )
 
